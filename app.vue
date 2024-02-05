9e6d189f31351346048749b2947a4c1501046718
lutfiahdewi <lutfiahdewi909@gmail.com>
2024-02-05 15:39:54 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+    yaa
+    <div class="tryn bg-sky-500">
+      coba tailwind
+    </div>
+  </div>
+</template>
+
