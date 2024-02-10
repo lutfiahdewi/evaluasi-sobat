e61c0050b7b303e977173647a2eaae8c1eab1193
lutfiahdewi <lutfiahdewi909@gmail.com>
2024-02-10 21:31:32 +0700

    dynamic layout prod ver

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,28 @@
 <script setup>
-definePageMeta({
-  layout: false,
-});
 useSeoMeta({
   title: "Beranda",
 });
 const authorizedRole = useCookie("authorizedRole");
-const activeBar = useCookie('activeBar');
-activeBar.value = 'Beranda'
+if (!authorizedRole.value) {
+  authorizedRole = "default";
+}
+definePageMeta({
+  layout: authorizedRole,
+});
+const activeBar = useCookie("activeBar");
+activeBar.value = "Beranda";
 </script>
 
 <template>
-  <NuxtLayout :name="authorizedRole">
-    <section>
-      <h1>Welcome to the homepage</h1>
-      Tulis sesuatu disini
-      <div class="my-96"></div>
-    </section>
-    <section id="galeri">
-      <h3>Galeri</h3>
-    </section>
-    <section id="sekilas">
-      <h4>Sekilas tentang SOBAT</h4>
-    </section>
-  </NuxtLayout>
+  <section>
+    <h1>Welcome to the homepage</h1>
+    Tulis sesuatu disini
+    <div class="my-96"></div>
+  </section>
+  <section id="galeri">
+    <h3>Galeri</h3>
+  </section>
+  <section id="sekilas">
+    <h4>Sekilas tentang SOBAT</h4>
+  </section>
 </template>
