607b5d27d1e5a06ba6a7f181cc177a5cdf3d1602
lutfiahdewi <lutfiahdewi909@gmail.com>
2024-05-29 04:14:50 +0700

    add build setting in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   hooks: {},
+  build: {
+    transpile: ["graphql"],
+  },
   devtools: { enabled: true },
   typescript: {
     // typeCheck: true,
